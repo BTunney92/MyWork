01f745b117acf32bedd2798a05e2b87c34929019
unknown <G00270683@gmit.ie>
2021-02-06 11:49:43 +0000

    Week3 lab #1

---
+++ firstvariables.py
@@ -0,0 +1,11 @@
+#Having the craic with variables
+#Author: Brendan Tunney (slightly confused)
+
+ageOfPatient = {}
+age = 28
+print (type(ageOfPatient))
+print (type(age))
+print ("you are "  +  str (age))
+NextYear = int(age) + 1
+print ("Next year you will be " + str(NextYear))
+
---
+++ testTypes.py
@@ -0,0 +1,21 @@
+#Lab 3.1 - Creating 5 variable types
+#Author: Brendan Tunney
+
+i = 3 
+fl = 3.5
+isa = True
+memo = 'how now brown cow'
+lots = []
+
+#Testing Variable Types are Correct
+
+print (type(i))
+print (type(fl))
+print (type(isa))
+print (type(memo))
+print (type(lots))
+print('variable {} is of type:{}and value:{}'.format('i',type(i),i))
+print('variable {} is of type:{}and value:{}'.format('fl',type(fl),fl))
+print('variable {} is of type:{}and value:{}'.format('isa',type(isa),isa))
+print('variable {} is of type:{}and value:{}'.format('memo',type(memo),memo))
+print('variable {} is of type:{}and value:{}'.format('lots',type(lots),lots))
